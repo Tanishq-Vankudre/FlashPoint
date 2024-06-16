7a22243ef0e4e605bb6ecbb394a1d20993259436
Sanket Patil <99712085+sankettpatil@users.noreply.github.com>
2024-06-16 22:12:55 +0530

    Update NewsBoard.jsx

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -1,19 +1,33 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import Newsitem from "./Newsitem";
+import './NewsBoard.css'; // Import the CSS file
 
-const NewsBoard = ({category}) => {
-    const[articles,setArticles]=useState([]);
-    useEffect(()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
-        fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
-  },[category])
+const NewsBoard = ({ category, searchQuery }) => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+    if (searchQuery) {
+      url = `https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${import.meta.env.VITE_API_KEY}`;
+    }
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const filteredArticles = data.articles.filter(article => article.urlToImage); // Filter out articles without images
+        setArticles(filteredArticles);
+      });
+  }, [category, searchQuery]);
+
   return (
-    <div>
-        <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
-        {articles.map((news,index)=>{
-            return <Newsitem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
-        })}
+    <div className="newsboard-container">
+      <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
+      <div className="news-cards-container">
+        {articles.map((news, index) => (
+          <Newsitem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
+        ))}
+      </div>
     </div>
-  )
-}
-export default NewsBoard
+  );
+};
+
+export default NewsBoard;
