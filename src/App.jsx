4e921755cab14369d9dc13940571bb3a21e15729
Sanket Patil <99712085+sankettpatil@users.noreply.github.com>
2024-06-16 22:11:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react"
-import Navbar from "./components/Navbar"
-import NewsBoard from "./components/NewsBoard"
+// src/App.jsx
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import NewsBoard from "./components/NewsBoard";
 
 const App = () => {
-  const [category,setCategory] = useState("general");
+  const [category, setCategory] = useState("general");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
   return (
     <div>
-     <Navbar setCategory={setCategory}/>
-     <NewsBoard category={category}/>
+      <Navbar setCategory={setCategory} setSearchTerm={handleSearch} />
+      <div className="container my-4">
+        <NewsBoard category={category} searchQuery={searchQuery} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
